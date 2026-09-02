370b0d04ca37f4afad265e45da33fc206fa4b6c9
W0lfHack <krosh.super@mail.ru>
2026-09-03 00:05:27 +0300

    generator + discriminator

--- generator.py
+++ generator.py
@@ -5,27 +5,53 @@ from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 
 class Generator(nn.Module):
-    def __init__(self, input_size, output_size):
+    def __init__(self):
         super(Generator, self).__init__()
 
-        self.model = nn.Sequential(
-            nn.Linear(input_size, output_size),
-            nn.Tanh(),
-        )
+        self.layer1 = nn.Sequential(nn.Linear(in_features=100, out_features=256),
+                                    nn.LeakyReLU())
+        self.layer2 = nn.Sequential(nn.Linear(in_features=256, out_features=512),
+                                            nn.LeakyReLU())
+        self.layer3 = nn.Sequential(nn.Linear(in_features=512, out_features=1024),
+                                            nn.LeakyReLU())
+        self.output = nn.Sequential(nn.Linear(in_features=1024, out_features=28*28),
+                                    nn.LeakyReLU())
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.output(x)
+        return x
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        self.layer1 = nn.Sequential(nn.Linear(in_features=784, out_features=1024),
+                                    nn.LeakyReLU())
+        self.layer2 = nn.Sequential(nn.Linear(in_features=1024, out_features=512),
+                                    nn.LeakyReLU())
+        self.layer3 = nn.Sequential(nn.Linear(in_features=512, out_features=256),
+                                    nn.LeakyReLU())
+        self.output = nn.Sequential(nn.Linear(in_features=256, out_features=1),
+                                    nn.LeakyReLU())
 
     def forward(self, x):
-        img = self.model(x)
-        print(img.shape)
-        return img
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.output(x)
+        return x
+
+        
 
 def main():
-    transform = transforms.ToTensor()
+    transform = transforms.Compose([transforms.ToTensor(),
+                                    transforms.Normalize([0.5],[0.5])])
     mnist_trainset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
     mnist_testset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
 
-    print(mnist_trainset)
-    print(len(mnist_testset))
-    generator = Generator(100, 784)
+    generator = Generator()
     x = torch.randn(1, 100)
     generator(x)
 
