0d3c38677a1feaada98415deaf388d30bd7e4c63
W0lfHack <krosh.super@mail.ru>
2026-09-04 00:08:24 +0300

    feat: train_step for models

--- generator.py
+++ generator.py
@@ -51,9 +51,10 @@ def dataloader():
     mnist_trainset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
     mnist_testset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
 
-    dataloader_testset = DataLoader(mnist_testset, batch_size=64, shuffle=True)
+    dataloader_testset = DataLoader(mnist_testset, batch_size=64, shuffle=False)
     dataloader_trainset = DataLoader(mnist_trainset, batch_size=64, shuffle=True)
     return (dataloader_testset, dataloader_trainset)
+
 def main():
     dataloader_testset = dataloader()[0]
     dataloader_trainset = dataloader()[1]
@@ -64,7 +65,69 @@ def main():
     optimizerG = torch.optim.Adam(G.parameters())
     optimizerG = torch.optim.Adam(D.parameters())
 
-    
+def train_step_discriminator(epoch_index,     tb_writer, 
+                             training_loader,           generator,
+                             discriminator, g_optimizer, 
+                             d_optimizer):
+    running_loss_d = 0
+    last_loss_d = 0
+    running_loss_g = 0
+    last_loss_g = 0
+
+    for i, data in enumerate(training_loader):
+        inputs, _ = data
+        inputs = inputs.view(inputs.size(0), -1) 
+
+        # Discriminator training
+        d_optimizer.zero_grad()
+
+        outputs_d = discriminator(inputs)
+
+        targets_ones = torch.ones_like(outputs_d).to(outputs_d.device)
+        noise = torch.randn(inputs.size(0), 100).to(inputs.device)
+
+        fake_images = generator(noise)
+
+        outputs_fake = discriminator(fake_images.detach())
+        targets_zeros = torch.zeros_like(outputs_fake).to(outputs_fake.device)
+
+        criterion = nn.BCEWithLogitsLoss()
+
+        loss_ones = criterion(outputs_d, targets_ones)
+        loss_zeros = criterion(outputs_fake, targets_zeros)
+        loss_d = loss_ones + loss_zeros
+
+        loss_d.backward()
+        d_optimizer.step()
+
+        running_loss_d += loss_d.item()
+        if i % 1000 == 999:
+            last_loss_d = running_loss_d / 1000 # loss per batch
+            print(f'  batch {i + 1} loss: {last_loss_d}')
+            tb_x = epoch_index * len(training_loader) + i + 1
+            tb_writer.add_scalar('Loss/train', last_loss_d, tb_x)
+            running_loss_d = 0.
+
+        # Generator training
+        g_optimizer.zero_grad()
+
+        outputs_fake_for_g = discriminator(fake_images)
+        targets_ones_g = torch.ones_like(outputs_fake_for_g).to(outputs_fake_for_g.device)
+        
+        loss_g = criterion(outputs_fake_for_g, targets_ones_g)
+
+        loss_g.backward()
+        g_optimizer.step()
+
+        running_loss_g += loss_g.item()
+        if i % 1000 == 999:
+            last_loss_g = running_loss_g / 1000 # loss per batch
+            print(f'  batch {i + 1} loss: {last_loss_g}')
+            tb_x = epoch_index * len(training_loader) + i + 1
+            tb_writer.add_scalar('Loss/train', last_loss_g, tb_x)
+            running_loss_g = 0.
+
+    return (last_loss_g, last_loss_d)
 
 if __name__ == "__main__":
     main()
